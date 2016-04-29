9968e1daa4c08d55a23b75c4cfed8af2e8ffff90
Smith Dhumbumroong <zodmaner@gmail.com>
2016-04-30 05:06:58 +0700

    Adds a main function that can be used to start the server from a saved image

--- cu-openstack.lisp
+++ cu-openstack.lisp
@@ -19,3 +19,20 @@ the server."
 the start-server function and can be used to stop the said active acceptor
 and shutdown the server."
       (hunchentoot:stop active-acceptor))))
+
+(defun main ()
+  "Initializes the database and starts the server using the start-easy-server
+function. This is intended to be used as the toplevel function when creating a
+Lisp image, and the join-thread function call is needed to prevent the image
+from exiting right after starting the server."
+  (let* ((port-argv (second sb-ext:*posix-argv*))
+         (port (if (null port-argv)
+                   8080
+                   (parse-integer port-argv))))
+    (models:init-db)
+    (start-easy-server port)
+    (bt:join-thread (find-if #'(lambda (thread)
+                                 (string= (bt:thread-name thread)
+                                          (format nil "hunchentoot-listener-*:~A"
+                                                  port)))
+                             (bt:all-threads)))))
